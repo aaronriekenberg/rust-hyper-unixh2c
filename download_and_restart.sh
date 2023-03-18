0222c527852b36731c50150a13b5e29b1472b350
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2023-03-18 10:10:39 -0500

    Add download_and_restart.sh.

---
+++ download_and_restart.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+
+cd ~/rust-hyper-unixh2c
+
+systemctl --user stop rust-hyper-unixh2c.service
+
+git pull -v
+
+RELEASE=$(git describe --abbrev=0 --tags # gets tag from current branch)
+echo "RELEASE=$RELEASE"
+
+rm -fr target
+mkdir -p target/release
+cd target/release
+
+wget https://github.com/aaronriekenberg/rust-hyper-unixh2c/releases/download/${RELEASE}/rust-hyper-unixh2c-aarch64-unknown-linux-gnu.tar.gz
+
+tar xvf rust-hyper-unixh2c-aarch64-unknown-linux-gnu.tar.gz
+
+systemctl --user restart rust-hyper-unixh2c.service
