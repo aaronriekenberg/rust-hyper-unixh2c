ad650560abcb7532ecbe62028d59a4496ae13a7f
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2023-03-25 15:39:07 -0500

    Add vergen.

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,25 @@ mod time;
 
 use anyhow::Context;
 
+use tracing::info;
+
+fn log_vergen_info() {
+    info!("Vergen Info:");
+    info!("Git Describe: {}", env!("VERGEN_GIT_DESCRIBE"));
+    info!("Git SHA: {}", env!("VERGEN_GIT_SHA"));
+    info!(
+        "Cargo Target Triple: {}",
+        env!("VERGEN_CARGO_TARGET_TRIPLE")
+    );
+    info!("Build Timestamp: {}", env!("VERGEN_BUILD_TIMESTAMP"));
+    info!("Rustc Semver: {}", env!("VERGEN_RUSTC_SEMVER"));
+    info!("Sysinfo Name: {}", env!("VERGEN_SYSINFO_NAME"));
+    info!(
+        "Sysinfo CPU Core Count: {}",
+        env!("VERGEN_SYSINFO_CPU_CORE_COUNT"),
+    );
+}
+
 fn app_name() -> String {
     std::env::args().nth(0).unwrap_or("[UNKNOWN]".to_owned())
 }
@@ -17,6 +36,8 @@ fn app_name() -> String {
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
+
+    log_vergen_info();
 
     let config_file = std::env::args().nth(1).with_context(|| {
         format!(
