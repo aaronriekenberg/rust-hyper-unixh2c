be1bde153281d72f039aaede7cb1d796e4b4eb10
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2023-03-26 08:42:04 -0500

    Add create_release.sh

---
+++ scripts/create_release.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -x
+
+if [ $# -ne 1 ]; then
+  echo "usage: $0 <release version>"
+  exit 1
+fi
+
+RELEASE_VERSION=$1
+RELEASE_VERSION_WITHOUT_V=$(echo $RELEASE_VERSION | sed -e 's/^v//g')
+
+echo "RELEASE_VERSION=$RELEASE_VERSION"
+echo "RELEASE_VERSION_WITHOUT_V=$RELEASE_VERSION_WITHOUT_V"
+
+cd ~/rust-hyper-unixh2c
+
+toml set Cargo.toml package.version $RELEASE_VERSION_WITHOUT_V > Cargo.toml.tmp
+mv Cargo.toml.tmp Cargo.toml
+
+cargo build -v
+RESULT=$?
+echo "cargo build RESULT = $RESULT"
+if [ $RESULT -ne 0 ]; then
+  echo "cargo build failed"
+fi
+
+cargo test -v
+RESULT=$?
+echo "cargo test RESULT = $RESULT"
+if [ $RESULT -ne 0 ]; then
+  echo "cargo test failed"
+fi
+
+git add Cargo.toml Cargo.lock
+
+git commit -m "Version $RELEASE_VERSION_WITHOUT_V"
+
+git tag $RELEASE_VERSION
+
+git push --all --tags
