30409aec6d0b9e870b7f43d9d7fe5d3a77971fc0
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2023-04-10 18:15:37 -0500

    Remove sysinfo fields.

--- src/version.rs
+++ src/version.rs
@@ -17,25 +17,5 @@ pub fn get_verison_info() -> VersionInfoMap {
     map.insert("VERGEN_RUSTC_CHANNEL", env!("VERGEN_RUSTC_CHANNEL"));
     map.insert("VERGEN_RUSTC_SEMVER", env!("VERGEN_RUSTC_SEMVER"));
 
-    map.insert("VERGEN_SYSINFO_NAME", env!("VERGEN_SYSINFO_NAME"));
-    map.insert(
-        "VERGEN_SYSINFO_OS_VERSION",
-        env!("VERGEN_SYSINFO_OS_VERSION"),
-    );
-    map.insert(
-        "VERGEN_SYSINFO_CPU_VENDOR",
-        env!("VERGEN_SYSINFO_CPU_VENDOR"),
-    );
-    map.insert(
-        "VERGEN_SYSINFO_CPU_CORE_COUNT",
-        env!("VERGEN_SYSINFO_CPU_CORE_COUNT"),
-    );
-    map.insert("VERGEN_SYSINFO_CPU_NAME", env!("VERGEN_SYSINFO_CPU_NAME"));
-    map.insert("VERGEN_SYSINFO_CPU_BRAND", env!("VERGEN_SYSINFO_CPU_BRAND"));
-    map.insert(
-        "VERGEN_SYSINFO_CPU_FREQUENCY",
-        env!("VERGEN_SYSINFO_CPU_FREQUENCY"),
-    );
-
     map
 }
