d4beadfbe13238293696af43517e11746a56f3a2
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2023-04-22 11:19:50 -0500

    Convert json keys to lowercase.

--- src/handlers/version_info.rs
+++ src/handlers/version_info.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{collections::BTreeMap, path::PathBuf};
 
 use async_trait::async_trait;
 
@@ -15,6 +15,11 @@ struct VersionInfoHandler;
 impl RequestHandler for VersionInfoHandler {
     async fn handle(&self, _request: &HttpRequest) -> Response<Body> {
         let version_info = get_verison_info().await;
+
+        let version_info: BTreeMap<String, &str> = version_info
+            .iter()
+            .map(|(k, v)| (k.to_lowercase(), *v))
+            .collect();
 
         build_json_response(version_info)
     }
