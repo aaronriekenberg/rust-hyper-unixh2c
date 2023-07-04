9142bcb1e2d6f64f866579d6bebd2cc20288ae56
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2023-07-04 09:09:41 -0500

    Add -e flag to fail on any error.

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash -x -e
 
 if [ $# -ne 1 ]; then
   echo "usage: $0 <release version>"
@@ -22,34 +22,22 @@ cd ~/rust-hyper-server
 toml set Cargo.toml package.version $RELEASE_VERSION_WITHOUT_V > Cargo.toml.tmp
 RESULT=$?
 echo "toml set test RESULT = $RESULT"
-if [ $RESULT -ne 0 ]; then
-  echo "toml set failed"
-  exit 1
-fi
 
 mv Cargo.toml.tmp Cargo.toml
 
 cargo build -v
 RESULT=$?
 echo "cargo build RESULT = $RESULT"
-if [ $RESULT -ne 0 ]; then
-  echo "cargo build failed"
-  exit 1
-fi
 
 cargo test -v
 RESULT=$?
 echo "cargo test RESULT = $RESULT"
-if [ $RESULT -ne 0 ]; then
-  echo "cargo test failed"
-  exit 1
-fi
 
-git add Cargo.toml Cargo.lock || exit 1
+git add Cargo.toml Cargo.lock
 
-git commit -m "Version $RELEASE_VERSION_WITHOUT_V" || exit 1
+git commit -m "Version $RELEASE_VERSION_WITHOUT_V"
 
-git tag $RELEASE_VERSION || exit 1
+git tag $RELEASE_VERSION
 
-git push -v || exit 1
-git push -v --tags || exit 1
+git push -v
+git push -v --tags
