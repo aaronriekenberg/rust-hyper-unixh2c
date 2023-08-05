4d3203ffb291e65f84aeca855c0f4310f65a8eb2
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2023-08-05 10:35:13 -0500

    Different log formats for dev and prod.

--- src/main.rs
+++ src/main.rs
@@ -45,10 +45,31 @@ async fn try_main() -> anyhow::Result<()> {
 
     server.run().await
 }
+fn initialize_tracing_subscriber() {
+    use tracing_subscriber::{filter::LevelFilter, fmt, prelude::*, EnvFilter};
+
+    let env_filter = EnvFilter::builder()
+        .with_default_directive(LevelFilter::INFO.into())
+        .from_env_lossy();
+
+    let log_format_value = std::env::var("LOG_FORMAT").unwrap_or_else(|_| "dev".to_string());
+
+    if log_format_value.eq_ignore_ascii_case("prod") {
+        tracing_subscriber::registry()
+            .with(env_filter)
+            .with(fmt::layer().with_ansi(false).without_time())
+            .init();
+    } else {
+        tracing_subscriber::registry()
+            .with(env_filter)
+            .with(fmt::layer())
+            .init();
+    };
+}
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::init();
+    initialize_tracing_subscriber();
 
     if let Err(err) = try_main().await {
         error!("fatal error in main:\n{:#}", err);
