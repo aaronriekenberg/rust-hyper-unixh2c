c737570cb665b9761705baa36541199052267839
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2023-08-27 09:00:22 -0500

    Add cargo_debug, cargo_opt_level.

--- src/version.rs
+++ src/version.rs
@@ -9,11 +9,16 @@ async fn build_version_info_map() -> VersionInfoMap {
 
     map.insert("build_timestamp", env!("VERGEN_BUILD_TIMESTAMP"));
 
+    map.insert("cargo_debug", env!("VERGEN_CARGO_DEBUG"));
+
+    map.insert("cargo_opt_level", env!("VERGEN_CARGO_OPT_LEVEL"));
+
     map.insert("cargo_pkg_version", env!("CARGO_PKG_VERSION"));
 
     map.insert("cargo_target_triple", env!("VERGEN_CARGO_TARGET_TRIPLE"));
 
     map.insert("rustc_channel", env!("VERGEN_RUSTC_CHANNEL"));
+
     map.insert("rustc_semver", env!("VERGEN_RUSTC_SEMVER"));
 
     map
