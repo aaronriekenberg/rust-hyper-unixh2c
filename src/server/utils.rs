cbbdb7ac2d24e966aeca978bbae126e6183352c1
Aaron.Riekenberg <aaron.riekenberg@target.com>
2023-12-22 15:27:18 -0600

    Remove unused types.

--- src/server/utils.rs
+++ src/server/utils.rs
@@ -1,19 +1,3 @@
-use hyper::service::HttpService;
-
-use std::convert::Infallible;
-
-use crate::response::ResponseBody;
-
 pub trait HyperReadWrite: hyper::rt::Read + hyper::rt::Write + Send + Unpin + 'static {}
 
 impl<T> HyperReadWrite for T where T: hyper::rt::Read + hyper::rt::Write + Send + Unpin + 'static {}
-
-pub trait HyperHttpService:
-    HttpService<hyper::body::Incoming, ResBody = ResponseBody, Error = Infallible>
-{
-}
-
-impl<T> HyperHttpService for T where
-    T: HttpService<hyper::body::Incoming, ResBody = ResponseBody, Error = Infallible>
-{
-}
