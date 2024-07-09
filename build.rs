46c41647dca7abff5d6bfc7992adcb63f9243569
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2024-07-09 18:10:58 -0500

    cargo update.  Update vergen to version 9.

--- build.rs
+++ build.rs
@@ -1,13 +1,13 @@
 use std::error::Error;
-use vergen::EmitBuilder;
+
+use vergen::{BuildBuilder, CargoBuilder, Emitter, RustcBuilder, SysinfoBuilder};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // Emit the instructions
-    EmitBuilder::builder()
-        .all_build()
-        .all_cargo()
-        .all_rustc()
-        .all_sysinfo()
+    Emitter::default()
+        .add_instructions(&BuildBuilder::all_build()?)?
+        .add_instructions(&CargoBuilder::all_cargo()?)?
+        .add_instructions(&RustcBuilder::all_rustc()?)?
+        .add_instructions(&SysinfoBuilder::all_sysinfo()?)?
         .emit()?;
     Ok(())
 }
